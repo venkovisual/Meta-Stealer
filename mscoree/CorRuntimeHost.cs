64fc38d323abe8ca0297b6fd88fa5fc9f2e9ead5
rakium <karenoferlc4+temp@gmail.com>
2024-11-08 19:58:59 +0000

    Add files via upload

---
+++ mscoree/CorRuntimeHost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace mscoree
+{
+	// Token: 0x020002EB RID: 747
+	[Guid("CB2F6722-AB3A-11D2-9C40-00C04FA30A3E")]
+	[TypeIdentifier]
+	[CompilerGenerated]
+	[CoClass(typeof(object))]
+	[ComImport]
+	public interface CorRuntimeHost : ICorRuntimeHost
+	{
+	}
+}
---
+++ mscoree/ICorRuntimeHost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace mscoree
+{
+	// Token: 0x020002EC RID: 748
+	[TypeIdentifier]
+	[Guid("CB2F6722-AB3A-11D2-9C40-00C04FA30A3E")]
+	[CompilerGenerated]
+	[InterfaceType(1)]
+	[ComImport]
+	public interface ICorRuntimeHost
+	{
+		// Token: 0x06001364 RID: 4964
+		void _VtblGap1_11();
+
+		// Token: 0x06001365 RID: 4965
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		void EnumDomains(out IntPtr hEnum);
+
+		// Token: 0x06001366 RID: 4966
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		void NextDomain([In] IntPtr hEnum, [MarshalAs(UnmanagedType.IUnknown)] out object pAppDomain);
+
+		// Token: 0x06001367 RID: 4967
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		void CloseEnum([In] IntPtr hEnum);
+	}
+}
